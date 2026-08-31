591ee1572fde71fc976504638b0e13e5c3f7dd60
Fabiano <git@git.com.br>
2026-08-31 15:22:38 -0300

    Add image

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -15,7 +15,7 @@ var portifolio = {
         "email": "fabiuniz@msn.com",
         "linkedin": "https://www.linkedin.com/in/fabiano-rocha-de-almeida-dev",
         "github": "https://github.com/fabiuniz",
-        "qrcode":"https://raw.githubusercontent.com/fabiuniz/portifolio/main/portifolio/assets/img/qr-profile.png"
+        "qrcode": "https://raw.githubusercontent.com/fabiuniz/portifolio/main/assets/img/qr-profile.png"
     },
     "about": {
         "description": [
